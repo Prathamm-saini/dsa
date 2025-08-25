c08b065d37d5abe68ed4b642c6899c1a6631ed84
Prathamm-saini <prathamzxc@gmail.com>
2025-08-25 16:46:01 +0530

    Concept questions added

--- src/basics/Recursion/intro.java
+++ src/basics/Recursion/intro.java
@@ -29,8 +29,14 @@ package basics.Recursion;
 /// in the end main moves out of stack and function gets finished
 public class intro {
     public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
         print(1);
         numberProgram(1);
+        System.out.println();
+        iterativeFibonacci(10);
+        System.out.println();
+        System.out.println(recursiveFibonacci(10));
+        System.out.println(recursiveBinarySearch(array,8,0,array.length-1));
     }
     // Violates the DRY principle
     static void message(int n){
@@ -54,5 +60,57 @@ public class intro {
         System.out.print(n+" ");
         numberProgram(++n);
     }
-
+    static void iterativeFibonacci(int n){
+        if(n==0){
+            System.out.println(0);
+            return;
+        }
+        int prev = 0;
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(prev+" ");
+            int next =  prev + curr;
+            prev = curr;
+            curr = next;
+        }
+    }
+    static int recursiveFibonacci(int n){
+        if(n == 0) return 0;
+        if(n <= 1){
+            return n;
+        }
+        return recursiveFibonacci(n-1)+recursiveFibonacci(n-2);
+    }
+    static int binarySearch(int[] array,int target){
+        int low = 0;
+        int high = array.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(array[mid] == target){
+                return mid;
+            }
+            else if(array[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+    static int recursiveBinarySearch(int[] array,int target,int low,int high){
+        if(low>high){
+            return -1;
+        }
+        int mid = low + (high-low)/2;
+        if(array[mid] == target){
+            return mid;
+        }
+        else if(array[mid] < target){
+            return recursiveBinarySearch(array,target,mid +1,high);
+        }
+        else{
+            return recursiveBinarySearch(array,target,low,mid-1);
+        }
+    }
 }
