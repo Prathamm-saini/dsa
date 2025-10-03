6d81704c55c51d276fba1e75d14bd0629681dbc8
Prathamm-saini <prathamzxc@gmail.com>
2025-10-03 19:58:12 +0530

    Valid parentheses added in Stack Question's Package

---
+++ src/basics/Stack/Questions/validParentheses.java
@@ -0,0 +1,25 @@
+package basics.Stack.Questions;
+
+import java.util.Stack;
+
+public class validParentheses {
+    public static void main(String[] args) {
+
+    }
+    public static boolean ValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i <s.length()-1; i++) {
+            if(s.charAt(i) == '(' || s.charAt(i)== '{' || s.charAt(i) =='['){
+                stack.push(s.charAt(i));
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if(s.charAt(i) == ')' && top != '('){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
